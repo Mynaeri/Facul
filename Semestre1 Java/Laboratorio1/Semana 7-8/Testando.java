24e51893eee65390c243a5fa705df2ff9f6e56e6
Mynaeri <hoxxyyt@gmail.com>
2025-12-05 08:48:38 -0300

    mais exercicios

---
+++ Semestre1 Java/Laboratorio1/Semana 7-8/Testando.java
@@ -0,0 +1,17 @@
+public class Testando{
+    public static void main (String [] args){
+    }
+}
+
+
+
+/*
+        while (i<= 5)
+            System.out.println("*".repeat(i));
+            i++;
+*/
+
+/*
+        for (int i = 1; i <= 5; i++)
+            System.out.println("*".repeat(i));
+*/
